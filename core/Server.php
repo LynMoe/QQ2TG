aa377821a0e4cee7b28fa84b763c4fdc5b39c321
XiaoLin <i@cgl.li>
2018-07-12 16:22:58 +0800

    修复小BUG

--- core/Server.php
+++ core/Server.php
@@ -39,6 +39,7 @@ class Server
          * [Main]客户端发送消息时
          */
         $server->on('Message', function (swoole_websocket_server $server, $frame) use($int) {
+            global $int;
             if (($int = $int + 1) >= CONFIG['restart_count']) exit("\n\n计数达到 " . $int . " , 结束进程\n\n");
             echo "-----------[{$int}]----------\n";
             $data = json_decode($frame->data,true);
