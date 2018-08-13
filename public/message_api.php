21b5b3ba4b0390fe0c93376cf4f0a1176ac3bbda
XiaoLin <i@cgl.li>
2018-08-13 19:30:39 +0800

    修复参数键名错误

--- public/message_api.php
+++ public/message_api.php
@@ -58,7 +58,7 @@ if (isset($_GET['user_id']) && isset($_GET['group_id']) && isset($_GET['time']) 
                 switch ($result['type'])
                 {
                     case 'image':
-                        $result['data']['url'] = str_replace('https://gchat.qpic.cn',CONFIG['photo']['proxy'],$result['data']['url']);
+                        $result['data']['url'] = str_replace('https://gchat.qpic.cn',CONFIG['image']['proxy'],$result['data']['url']);
                         $data[$key]['message'] = str_replace($item,'<img src="' . $result['data']['url'] . '">',$data[$key]['message']);
                         break;
 
