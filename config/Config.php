29e9c1f588a1964430823befb711029ac0289898
XiaoLin <i@cgl.li>
2018-08-21 22:58:54 +0800

    新增指定发送对话功能

--- config/Config.php
+++ config/Config.php
@@ -9,5 +9,6 @@
 if (!file_exists(__DIR__ . '/../config/.env')) die('请先完成设置');
 $config = array_merge(parse_ini_file(__DIR__ . '/../config/.env.example',true),parse_ini_file(__DIR__ . '/../config/.env',true));
 $config['image']['folder'] = __DIR__ . $config['image']['folder'];
+if ($config['admin']['send_to'] == '') $config['admin']['send_to'] = $config['admin']['chat_id'];
 define('CONFIG',$config);
 unset($config);
